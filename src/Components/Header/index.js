6f1b1a62d16f6ceafd5cc4cca13098824410c7f4
AgustinJaime <ajaime.dev@gmail.com>
2020-07-26 18:13:41 +0200

    Header

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,7 +1,14 @@
 import React from "react";
+import classes from "./Header.module.css";
 
 const Header = () => {
-  return <div>Hi jskdjsakl</div>;
+  return (
+    <div className={classes.HeaderWrapper}>
+      {/* <h2>Welcome to</h2> */}
+      <h1>brastlewark town</h1>
+      <p>Where the journey begins</p>
+    </div>
+  );
 };
 
 export default Header;
