7d1f75f07bb35b7c53c0c6c8e88fcc856e25ba5c
AgustinJaime <ajaime.dev@gmail.com>
2020-07-26 18:01:31 +0200

    Text style. Header comp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 
+import Header from "./Components/Header";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Header/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <div>Hi jskdjsakl</div>;
+};
+
+export default Header;
