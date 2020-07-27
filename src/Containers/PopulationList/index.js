5a87c432d7a5be2e4ca6b30b0f826a1844d6e939
AgustinJaime <ajaime.dev@gmail.com>
2020-07-27 03:05:08 +0200

    Search by name. Pagination

--- src/Containers/PopulationList/index.js
+++ src/Containers/PopulationList/index.js
@@ -8,10 +8,11 @@ import ItemDetail from "../../Components/ItemDetail";
 const PopulationList = () => {
   const [data, setData] = useState(null);
   const [page, setPage] = useState(1);
-  const [pagination, setPagination] = useState([]);
   const [selectedItem, setSelectedItem] = useState({});
   const [isSelected, setIsSelected] = useState(false);
   const [noResults, setNoResults] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+  const [filteredItems, setFilteredItems] = useState(data);
 
   useEffect(() => {
     axios
@@ -24,13 +25,35 @@ const PopulationList = () => {
       });
   }, []);
 
-  useEffect(() => {
-    setPagination(data ? data.slice((page - 1) * 10, page * 10) : []);
-  }, [data, page]);
-
   const paginationHandler = (val) => {
-    setPage(val === "+" ? page + 1 : page - 1);
+    if (val === "+") {
+      setPage(page + 1);
+    } else if (val === "-") {
+      setPage(page === 1 ? page : page - 1);
+    }
   };
+
+  console.log(page);
+
+  const pageResults = filteredItems
+    ? filteredItems.slice((page - 1) * 10, page * 10)
+    : [];
+
+  useEffect(
+    () =>
+      setFilteredItems(
+        data
+          ? data.filter((el) =>
+              el.name.toLowerCase().includes(inputValue.toLowerCase())
+            )
+          : data
+      ),
+    [data, inputValue]
+  );
+
+  useEffect(() => {
+    setNoResults(data ? (filteredItems.length === 0 ? true : false) : false);
+  }, [filteredItems]);
 
   const selectedItemHandler = (id) => {
     let selected = data ? data[id] : {};
@@ -38,8 +61,15 @@ const PopulationList = () => {
     setSelectedItem(selected);
   };
 
+  const searchByNameHandler = (event) => {
+    // console.log(event.target.value);
+    setIsSelected(false);
+    setPage(1);
+    setInputValue(event.target.value);
+  };
+
   const namesList = data ? (
-    pagination.map((el) => (
+    pageResults.map((el) => (
       <ListItem
         itemName={el.name}
         key={el.id}
@@ -59,6 +89,7 @@ const PopulationList = () => {
           className={classes.SearchInput}
           type="text"
           placeholder="Search by name!"
+          onChange={searchByNameHandler}
         />
         <h2>Filter</h2>
       </div>
@@ -71,6 +102,15 @@ const PopulationList = () => {
             noResults={noResults}
           />
         </div>
+      </div>
+      <div className={classes.PaginationContainer}>
+        <p onClick={() => paginationHandler("-")}>PREV</p>
+        <p>
+          {filteredItems && filteredItems.length > 0
+            ? `Page ${page} of ${Math.ceil(filteredItems.length / 10)}`
+            : "No Results"}
+        </p>
+        <p onClick={() => paginationHandler("+")}>NEXT</p>
       </div>
     </div>
   );
